60e27ab79864d68a13d9927cf3035b10122862bf
Pete Mills <mills.pete@gmail.com>
2019-03-25 23:58:52 -0400

    Section 2 complete.

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -10,12 +10,12 @@ import { getQueryValue } from '@angular/core/src/view/query';
 export class ValueComponent implements OnInit {
   values: any;
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getValues();
   }
-  
+
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
